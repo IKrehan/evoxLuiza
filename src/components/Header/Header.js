2b9e59a49a56c5c07d6f11437d009cd9fb8d5a6c
IKrehan <irvigkrehan@hotmail.com>
2020-11-17 20:03:06 -0300

    Header Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,37 @@ import './Header.css';
 import EvoxLogo from '../media/logo.svg'
 
 import Container from 'react-bootstrap/Container';
+import Nav from "react-bootstrap/Nav";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
  const Header = () => {
     return (
         <header>
             <Container className='py-5'>
-                <img src={EvoxLogo} alt="Evox Logo"/>
+                <Row>
+                    <Col md={6}>
+                        <img src={EvoxLogo} alt="Evox Logo"/>
+                    </Col>
+                    
+                    <Col md={6} className="d-flex justify-content-end">
+                        <Nav activeKey="/">
+                            <Nav.Item>
+                                <Nav.Link href="/">
+                                    <span className="navlink active">Listagem</span>
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link eventKey="add">
+                                    <span className="navlink">Adicionar Produto</span>
+                                    </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                </Row>
+                
             </Container>
         </header>
     );
